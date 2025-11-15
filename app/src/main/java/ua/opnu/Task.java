3d7eeafa536e7b64a41f8afc17c903efe75d3ae2
Maksym Zadorozhniuk <9175593@stud.op.edu.ua>
2025-11-15 13:15:26 +0200

    lab 9 complete

--- app/src/main/java/ua/opnu/Task.java
+++ app/src/main/java/ua/opnu/Task.java
@@ -1,9 +1,6 @@
 package ua.opnu;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 public class Task {
     public static void main(String[] args) {
@@ -11,75 +8,172 @@ public class Task {
     }
 
     public void removeShorterStrings(List<String> list) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        for (int i = 0; i < list.size() - 1; i++) {
+            String a = list.get(i);
+            String b = list.get(i + 1);
+
+            if (a.length() <= b.length()) {
+                list.remove(i);
+            } else {
+                list.remove(i + 1);
+            }
+        }
     }
 
     public void stutter(List<String> list) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        for (int i = 0; i < list.size(); i += 2) {
+            list.add(i, list.get(i));
+        }
     }
 
     public void switchPairs(List<String> list) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        for (int i = 0; i < list.size() - 1; i += 2) {
+            String tmp = list.get(i);
+            list.set(i, list.get(i + 1));
+            list.set(i + 1, tmp);
+        }
     }
 
     public void removeDuplicates(List<String> list) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        for (int i = 0; i < list.size() - 1; ) {
+            if (list.get(i).equals(list.get(i + 1))) {
+                list.remove(i + 1);
+            } else {
+                i++;
+            }
+        }
     }
 
     public void markLength4(List<String> list) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).length() == 4) {
+                list.add(i, "****");
+                i++;
+            }
+        }
     }
 
     public boolean isPalindrome(Queue<Integer> queue) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return false;
+        Stack<Integer> s = new Stack<>();
+        int size = queue.size();
+
+        for (int i = 0; i < size; i++) {
+            int v = queue.remove();
+            queue.add(v);
+            s.push(v);
+        }
+
+        boolean ok = true;
+
+        for (int i = 0; i < size; i++) {
+            int v = queue.remove();
+            if (v != s.pop()) ok = false;
+            queue.add(v);
+        }
+
+        return ok;
     }
 
     public void reorder(Queue<Integer> queue) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        Stack<Integer> s = new Stack<>();
+        while (!queue.isEmpty()) s.push(queue.remove());
+        ArrayList<Integer> list = new ArrayList<>(s);
+        list.sort(Integer::compare);
+        for (int x : list) queue.add(x);
     }
 
     public void rearrange(Queue<Integer> queue) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        Queue<Integer> evens = new LinkedList<>();
+        Queue<Integer> odds = new LinkedList<>();
+
+        while (!queue.isEmpty()) {
+            int x = queue.remove();
+            if (x % 2 == 0) evens.add(x);
+            else odds.add(x);
+        }
+
+        queue.addAll(evens);
+        queue.addAll(odds);
     }
 
     public int maxLength(Set<String> set) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return 0;
+        int max = 0;
+        for (String s : set) {
+            if (s.length() > max) max = s.length();
+        }
+        return max;
     }
 
     public void removeEvenLength(Set<String> set) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        set.removeIf(s -> s.length() % 2 == 0);
     }
 
     public int numInCommon(List<Integer> list1, List<Integer> list2) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return 0;
+        Set<Integer> s1 = new HashSet<>(list1);
+        Set<Integer> s2 = new HashSet<>(list2);
+        s1.retainAll(s2);
+        return s1.size();
     }
 
     public boolean isUnique(Map<String, String> map) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return false;
+        Set<String> vals = new HashSet<>(map.values());
+        return vals.size() == map.size();
     }
 
     public Map<String, Integer> intersect(Map<String, Integer> map1, Map<String, Integer> map2) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        Map<String, Integer> res = new HashMap<>();
+        for (String key : map1.keySet()) {
+            if (map2.containsKey(key) && map1.get(key).equals(map2.get(key))) {
+                res.put(key, map1.get(key));
+            }
+        }
+        return res;
     }
 
     public Map<String, Integer> reverse(Map<Integer, String> map) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        Map<String, Integer> res = new HashMap<>();
+        for (var e : map.entrySet()) {
+            res.put(e.getValue(), e.getKey());
+        }
+        return res;
     }
 
     public int rarest(Map<String, Integer> map) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return 0;
+        if (map.isEmpty()) return 0;
+
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int v : map.values()) {
+            freq.put(v, freq.getOrDefault(v, 0) + 1);
+        }
+
+        int bestVal = 0;
+        int bestCount = Integer.MAX_VALUE;
+
+        for (var e : freq.entrySet()) {
+            int value = e.getKey();
+            int count = e.getValue();
+
+            if (count < bestCount || (count == bestCount && value < bestVal)) {
+                bestCount = count;
+                bestVal = value;
+            }
+        }
+
+        return bestVal;
     }
 
     public int maxOccurrences(List<Integer> list) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return 0;
-    }
+        if (list.isEmpty()) return 0;
 
+        Map<Integer, Integer> freq = new HashMap<>();
+        int best = 0;
+
+        for (int x : list) {
+            int c = freq.getOrDefault(x, 0) + 1;
+            freq.put(x, c);
+            if (c > best) best = c;
+        }
+
+        return best;
+    }
 }
